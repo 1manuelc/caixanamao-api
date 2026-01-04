dfddd562e07aa00f4f1fdcf4b91472880f4bc1fb
Manuel C. <manuelwn21@gmail.com>
2026-01-04 00:53:27 -0300

    feat(auth): implementa modulo de autenticacao com login basico

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginDto } from './dtos/login.dto';
+
+import jwt from 'jsonwebtoken';
+
+@Injectable()
+export class AuthService {
+  constructor(private prismaService: PrismaService) {}
+  async login(body: LoginDto) {
+    const { email, senha } = body;
+
+    if (!email) {
+      throw new BadRequestException('O email é obrigatório!');
+    }
+
+    if (!senha) {
+      throw new BadRequestException('A senha é obrigatória!');
+    }
+
+    const user = await this.prismaService.tbusuario.findUnique({
+      where: { email },
+    });
+
+    if (user === null) {
+      throw new NotFoundException('Email e/ou senha incorretos!');
+    }
+
+    const checkPassword = senha == user.senha;
+
+    if (!checkPassword) {
+      throw new BadRequestException('Email e/ou senha incorretos!');
+    }
+
+    const expiresIn = 2400; //40min
+    const token = jwt.sign({ id: user.iduser }, process.env.SECRET_KEY!, {
+      expiresIn: expiresIn,
+    });
+
+    return token;
+  }
+}
