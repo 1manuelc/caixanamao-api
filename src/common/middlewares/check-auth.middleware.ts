b0b8052d46aa4ad2bee0410333aa3e8a0f12e582
Manuel C. <manuelwn21@gmail.com>
2026-01-07 18:16:00 -0300

    refactor(check-auth.middleware): refatora middleware para verificar somente a presença do authorization header

--- src/common/middlewares/check-auth.middleware.ts
+++ src/common/middlewares/check-auth.middleware.ts
@@ -1,6 +1,5 @@
 import { NestMiddleware, UnauthorizedException } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 
 export class CheckAuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
@@ -8,23 +7,11 @@ export class CheckAuthMiddleware implements NestMiddleware {
     const token = authorization?.split(' ')[1];
 
     if (!token) {
-      throw new UnauthorizedException('Acesso negado!');
+      throw new UnauthorizedException(
+        'Token de acesso não fornecido, acesso negado',
+      );
     }
 
-    if (
-      process.env.NODE_ENV === 'development' &&
-      token === process.env.MASTER_ACCESS_TOKEN
-    ) {
-      return next();
-    }
-
-    try {
-      jwt.verify(token, process.env.SECRET_KEY!);
-      next();
-    } catch (e) {
-      if (e instanceof Error) {
-        throw new UnauthorizedException('O Token é inválido!', e.message);
-      }
-    }
+    return next();
   }
 }
