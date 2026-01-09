ce9b92162ac5d95ab61d8c8c9c1a1e231ea7e8c0
Manuel C. <manuelwn21@gmail.com>
2026-01-09 17:24:33 -0300

    fix(CreateRegisterDto): remove regra errada para validação de datas

--- src/registers/dtos/create-register.dto.ts
+++ src/registers/dtos/create-register.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from 'class-transformer';
-import { IsDate, IsNumber, IsUUID, MaxDate } from 'class-validator';
+import { IsDate, IsNumber, IsUUID } from 'class-validator';
 
 export class CreateRegisterDto {
   @IsUUID()
@@ -26,12 +26,10 @@ export class CreateRegisterDto {
   valor_despesas!: number;
 
   @IsDate()
-  @MaxDate(new Date())
   @Type(() => Date)
   data!: Date;
 
   @IsDate()
-  @MaxDate(new Date())
   @Type(() => Date)
   data_final!: Date;
 }
